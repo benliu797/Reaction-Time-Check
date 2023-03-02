1de2ea8f56e737338e564bac54f81ac37a2c8a2b
BeENL <70254026+benliu797@users.noreply.github.com>
2023-03-02 03:58:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 // Define variables
-var startButton = document.getElementById("start-button");
-var testArea = document.getElementById("test-area");
+var squares = document.getElementsByClassName("square");
 var startTime, endTime, reactionTime;
 
-// Event listener for start button
-startButton.addEventListener("click", function() {
-	// Hide start button and show test area
-	startButton.style.display = "none";
-	testArea.style.display = "block";
-	
-	// Set random delay before changing color to green
-	setTimeout(function() {
-		testArea.style.backgroundColor = "green";
-		startTime = new Date();
-	}, Math.random()*5000 + 1000);
-});
+// Event listener for squares
+for (var i = 0; i < squares.length; i++) {
+	squares[i].addEventListener("click", function() {
+		endTime = new Date();
+		reactionTime = (endTime - startTime) / 1000;
+		alert("Your reaction time was " + reactionTime + " seconds.");
+		
+		// Reset game
+		resetGame();
+	});
+}
 
-// Event listener for test area click
-testArea.addEventListener("click", function() {
-	endTime = new Date();
-	reactionTime = (endTime - startTime) / 1000;
-	alert("Your reaction time was " + reactionTime + " seconds.");
-	
-	// Reset test
-	testArea.style.backgroundColor = "red";
-	testArea.style.display = "none";
-	startButton.style.display = "block";
-});
+// Function to start game
+function startGame() {
+	// Hide squares and set random delay before showing first square
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.display = "none";
+	}
+	setTimeout(function() {
+		// Show random square and start timer
+		var randomIndex = Math.floor(Math.random() * squares.length);
+		squares[randomIndex].style.display = "block";
+		startTime = new Date();
+	}, Math.random()*2000 + 1000);
+}
+
+// Function to reset game
+function resetGame() {
+	// Hide squares and reset styles
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.display = "none";
+		squares[i].style.backgroundColor = "#4CAF50";
+	}
+}
+
+// Start game on page load
+window.onload = function() {
+	startGame();
+};
